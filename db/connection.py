e74f4bf538c2064ba760423b7dbaaf0bec9fa58d
Shaunak Guharay <guharayshaunak75@gmail.com>
2021-12-13 16:40:42 +0100

    Add get_weatherData_bySummary() API

--- db/connection.py
+++ db/connection.py
@@ -41,3 +41,29 @@ def get_weatherData():
       print(err)
   else:
     cnx.close()
+
+def get_weatherData_bySummary(summary):
+  """
+  Query all available weather data for defined summary (for eg. Clear, Foggy etc.)
+  Return Type: Pandas Dataframe
+  """
+
+  try:
+    cnx = mysql.connector.connect(**config)
+    cursor = cnx.cursor()
+    cursor.execute("select * from weatherHistory where summary={}".format(summary))
+    records = cursor.fetchall()
+    
+    df = pd.DataFrame(records)
+    df.columns = cursor.column_names
+    return df
+
+  except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+      print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+      print("Database does not exist")
+    else:
+      print(err)
+  else:
+    cnx.close()
